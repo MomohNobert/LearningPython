d36cfd5e60f6b3c47dae7526c4335012166f4054
Momoh Nobert <okhai0112@gmail.com>
2020-06-15 15:32:08 +0100

    working with inheritance

---
+++ Classes/inheritance.py
@@ -0,0 +1,26 @@
+class Animal:
+    def talk(self): print('I have something to say.')
+    def walk(self): print('Hey! I\'m walkin here!')
+    def clothes(self): print('I have nice clothes')
+
+class Duck(Animal):
+    def quack(self):
+        print('Quaaack!')
+
+    def walk(self):
+        super().walk()
+        print('Walks like a duck.')
+
+class Dog(Animal):
+    pass
+
+def main():
+    donald = Duck()
+    donald.quack()
+    donald.walk()
+    donald.clothes()
+    
+    fido = Dog()
+    fido.walk()
+
+if __name__ == "__main__": main()
