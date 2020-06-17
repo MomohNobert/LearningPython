2baeb52297bd02a82a130e4b0dc92234fa36c730
Momoh Nobert <okhai0112@gmail.com>
2020-06-17 23:09:37 +0100

    created a web crawler, added page no. functionality

--- WebCrawler/crawler.py
+++ WebCrawler/crawler.py
@@ -4,14 +4,17 @@ from bs4 import BeautifulSoup
 def trade_spider(max_pages):
     page = 1
     while page <= max_pages:
-        url = 'https://www.jumia.com.ng/watches-sunglasses/?page=2'
+        url = 'https://www.jumia.com.ng/watches-sunglasses/?page=' +str(page)
         source_code = requests.get(url)
         plain_text = source_code.text
-        soup = BeautifulSoup(plain_text)
+        soup = BeautifulSoup(plain_text, features='html.parser')
 
         for link in soup.findAll('a', {'class': 'core'}):
             href = link.get('href')
+            title = link.string
             print(href)
+            print(title)
+            print()
         page += 1
 
 trade_spider(1)
