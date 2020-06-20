3334863d4c6cb32b51cf702ffe37d60824ed3ed4
Momoh Nobert <okhai0112@gmail.com>
2020-06-20 01:35:46 +0100

    intro to pillow, working with cropped images

--- Pillow/main.py
+++ Pillow/main.py
@@ -1,7 +1,12 @@
 from PIL import Image
 
 img = Image.open("leo.jpg")
-print(img.size)
-print(img.format)
+# print(img.size)
+# print(img.format)
 
-img.show()
+# 4 parameters. 1st 2, x and y co-ordinates of left corner.
+area = (100, 100, 300, 375)
+cropped_img = img.crop(area)
+
+# img.show()
+cropped_img.show()
