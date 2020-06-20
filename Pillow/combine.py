71928468c0eed397618039bb68db6f44acd7791d
Momoh Nobert <okhai0112@gmail.com>
2020-06-20 01:47:48 +0100

    intro to pillow, didn't work due to bad dimensions

--- Pillow/combine.py
+++ Pillow/combine.py
@@ -3,7 +3,7 @@ from PIL import Image
 first_img = Image.open('marie.jpg')
 second_img = Image.open('luffy.jpg')
 
-area = (50,50, 550,645)
+area = ( 50, 50, 550,645)
 print(second_img.size)
 first_img.paste(second_img, area)
 
